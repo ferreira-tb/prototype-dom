c09c70caa342dc578a0dec65d5d2a95fde366be4
Andrew Ferreira <andrew.shien2@gmail.com>
2024-08-21 20:06:26 -0300

    build: use vite

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import dts from 'vite-plugin-dts';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [dts({ rollupTypes: true })],
+  build: {
+    outDir: 'dist',
+    target: 'esnext',
+    emptyOutDir: true,
+    minify: false,
+    lib: {
+      name: 'prototypeDom',
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['iife'],
+      fileName: 'index'
+    }
+  }
+});
